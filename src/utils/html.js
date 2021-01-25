14c09c3708681b00fd0e21a4514bd698eff20b20
Marco Jansen <marco.jansen@gmx.us>
2021-01-25 16:08:24 +0800

    created basic file structure as per requirement

---
+++ src/utils/html.js
@@ -0,0 +1,12 @@
+  
+function injectCode(source, placeholder, replaceValue){
+    return source.replace(
+        placeholder,
+        replaceValue
+    );
+}
+
+module.exports = {
+    injectCode,
+    
+}
