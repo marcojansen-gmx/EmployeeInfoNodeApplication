63008e5bf7092591dc4b63ca7ccc85debd4c4e56
Marco Jansen <marco.jansen@gmx.us>
2021-01-26 17:30:57 +0800

    added class files and basic inquirer structure to index.js

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 const htmlHelpers = require('./src/utils/html');
-let employees = [
-    {
-        type: "Manager",
-        roomId: "12-A",
-        name: "Sam",
-        email: "sam@sam.com",
-    },
-    {
-        type: "Engineer",
-        github: "sam-ngu",
-        name: "Sam",
-        email: "sam@sam.com",
-    },
-];
+const inquirer = require('inquirer');
 
+    inquirer.prompt([
+        {
+            type: 'checkbox',
+            message: 'Which employee type are you entering?',
+            name: 'employeeType',
+            choices: ['Employee', 'Engineer', 'Intern', 'Manager', 'Quit to result'],
+        },
+    ]);
 
-// inquirer 
+    // keep asking the user to enter which employee type
+    selectionPromise.then((data) => {
+        switch (data) {
+            case 'Employee':
+                return new AddEmployee(data);
+            case 'Engineer':
+                return new AddEngineer(data);
+            case 'Intern':
+                return new AddIntern(data);
+            case 'Manager':
+                return new AddManager(data);
+            default:
+                return showFinalResult(data);
+        }
+    }
 
-// keep asking the user to enter which employee type
+fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+    
+});
 
 // ask question specific to emmployee type
-// ;
-
- const htmlPath = path.join(__dirname, "src", "html-templates", "main.html");
- const layout = fs.readFileSync(htmlPath, "utf-8");
+const htmlPath = path.join(__dirname, "src", "html-templates", "main.html");
+const layout = fs.readFileSync(htmlPath, "utf-8");
 htmlHelpers.injectCode(layout, '{{ code_injection }}', JSON.stringify(employees));
