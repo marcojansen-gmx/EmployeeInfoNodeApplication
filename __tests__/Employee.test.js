a012bb69a6b1678fd54d72dead39019a21126c0f
Marco Jansen <marco.jansen@gmx.us>
2021-01-31 19:29:33 +0800

    finalized jest testing programming

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,5 @@
 const Employee = require('../lib/Employee');
 
-// test("name of the test", function )
-	//function has new variables
-	//expectation of the test
-
 test("checks if new Employee works", () => {
 	const employee = new Employee();
 	expect(typeof(employee)).toBe("object");
@@ -15,16 +11,28 @@ test ("check if correct name is stored", () => {
 });    
 
 test ("check if correct id is stored", () => {
-    const employee = new Employee("Bob", "12", "bob@gmx.com");
-    expect (employee.id).toBe("12");
+    const employee = new Employee("Hans", "13", "hans@gmx.com");
+    expect (employee.id).toBe("13");
 });
 
 test ("check if correct email is stored", () => {
-    const employee = new Employee("Bob", "12", "bob@gmx.com");
-    expect (employee.email).toBe("bob@gmx.com");
+    const employee = new Employee("Jesse", "14", "jesse@gmx.com");
+    expect (employee.email).toBe("jesse@gmx.com");
 });
 
 test ("check getName func", () => {
-    const employee = new Employee("Bob", "12", "bob@gmx.com");
-    expect(employee.getName()).toBe("Bob");
+    const employee = new Employee("Carl", "15", "carl@gmx.com");
+    expect(employee.getName()).toBe("Carl");
 });
+
+test ("check getEmail func", () => {
+    const employee = new Employee("Matt", "16", "matt@gmx.com");
+    expect(employee.getEmail()).toBe("matt@gmx.com");
+});
+
+
+test ("check getRole func", () => {
+    const employee = new Employee("Sam", "17", "sam@gmx.com");
+    expect(employee.getRole()).toBe("Employee");
+});
+
